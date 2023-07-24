2762360eb826e8ba9eee5cebc3b3886da4c794ea
sifes <rakasha72232629@gmail.com>
2023-07-24 23:00:43 +0300

    burger

---
+++ src/main.js
@@ -0,0 +1,7 @@
+(function() {
+    document.getElementById("burger").addEventListener('click',(()=>{
+    document.querySelector('#burgerMenu').classList.toggle('hidden')
+    document.querySelector('#burgerMenu').classList.toggle('flex')
+
+}))
+})()
