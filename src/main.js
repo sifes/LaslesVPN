208bbbbe5a87e81bd5cafd9cea1ae8e79df0ac79
sifes <rakasha72232629@gmail.com>
2023-07-25 21:17:45 +0300

    slider

--- src/main.js
+++ src/main.js
@@ -3,3 +3,41 @@
     document.querySelector('#burgerMenu').classList.toggle('disabled')
 }))
 })()
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    slidesPerView: 3,
+    spaceBetween: 50,
+    
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets'
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next-custom',
+      prevEl: '.swiper-button-prev-custom',
+    },
+    breakpoints: {
+        // when window width is >= 320px
+        320: {
+          slidesPerView: 1,
+          spaceBetween: 0
+        },
+        // when window width is >= 480px
+        768: {
+          slidesPerView: 2,
+          spaceBetween: 20
+        },
+        // when window width is >= 640px
+        1024: {
+          slidesPerView: 3,
+          spaceBetween: 50
+        }
+      }
+  });
